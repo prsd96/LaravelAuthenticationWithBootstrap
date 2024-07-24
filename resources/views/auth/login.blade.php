dff9dcbb95b5badadc253094e50fdd374bb6a6ea
prsd96 <prasadirache96@gmail.com>
2024-07-24 21:44:36 +0530

    FormAlignments

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -32,7 +32,7 @@
                     <form method="POST" action="{{ route('checkUserLogin') }}">
                         @csrf
                         
-                        <div class="form-group">
+                        <div class="form-group mb-3">
                             <label for="email">{{ __('Email Address') }}</label>
                             <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
                             @error('email')
@@ -40,7 +40,7 @@
                             @enderror
                         </div>
                         
-                        <div class="form-group">
+                        <div class="form-group mb-3">
                             <label for="password">{{ __('Password') }}</label>
                         
                             <div class="input-group">
@@ -55,22 +55,25 @@
                             </div>
                         </div> 
                         
-                        <div class="form-check">
+                        <div class="form-group text-center mb-3">
                             <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
                             <label class="form-check-label" for="remember">
                                 {{ __('Remember Me') }}
                             </label>
                         </div>
                         
-                        <button type="submit" class="btn btn-primary">{{ __('Login') }}</button>
+                        
+                        <div class="text-center">
+                            <button type="submit" class="btn btn-primary text-center">{{ __('Login') }}</button>
+                        </div>
                     </form>
                 </div>
                 
                 <div class="card-footer">
                     <div class="text-center">
-                        <a href="{{ route('forgotPassword') }}">Forgot your password?</a>
+                        <a href="{{ route('forgotPassword') }}" class="text-decoration-none">Forgot your password?</a>
                         <br>
-                        <a href="{{ route('register') }}">Don't have an account? Register here</a>
+                        <a href="{{ route('register') }}" class="text-decoration-none">Don't have an account? Register here</a>
                     </div>
                 </div>
             </div>
--- resources/views/auth/registration/register.blade.php
+++ resources/views/auth/registration/register.blade.php
@@ -32,7 +32,7 @@
                     <form method="POST" action="{{route('users.store')}}">
                         @csrf
                         
-                        <div class="form-group">
+                        <div class="form-group mb-3">
                             <label for="first_name">{{ __('First Name') }}</label>
                             <input id="first_name" type="text" class="form-control @error('first_name') is-invalid @enderror" name="first_name" value="{{ old('first_name') }}" required autocomplete="first_name" autofocus>
                             @error('first_name')
@@ -40,7 +40,7 @@
                             @enderror
                         </div>
                         
-                        <div class="form-group">
+                        <div class="form-group mb-3">
                             <label for="last_name">{{ __('Last Name') }}</label>
                             <input id="last_name" type="text" class="form-control @error('last_name') is-invalid @enderror" name="last_name" value="{{ old('last_name') }}" required autocomplete="last_name">
                             @error('last_name')
@@ -48,7 +48,7 @@
                             @enderror
                         </div>
                         
-                        <div class="form-group">
+                        <div class="form-group mb-3">
                             <label for="mobile">{{ __('Mobile Number (10 digit Indian number)') }}</label>
                             <input id="mobile" type="text" class="form-control @error('mobile') is-invalid @enderror" name="mobile" value="{{ old('mobile') }}" required autocomplete="mobile">
                             @error('mobile')
@@ -56,7 +56,7 @@
                             @enderror
                         </div>
                         
-                        <div class="form-group">
+                        <div class="form-group mb-3">
                             <label for="email">{{ __('Email Address') }}</label>
                             <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
                             @error('email')
@@ -64,24 +64,24 @@
                             @enderror
                         </div>
                         
-                        <div class="form-group">
+                        <div class="form-group mb-3">
                             <label for="password">{{ __('Password') }}</label>
-                        
+                            
                             <div class="input-group">
                                 <input id="regPasswordInput" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
                                 @error('password')
                                 <span class="invalid-feedback" role="alert">{{ $message }}</span>
                                 @enderror
-                        
+                                
                                 <div class="input-group-append">
                                     <button class="btn btn-outline-secondary" type="button" id="regPasswordButton" onclick="passwordShowHide('regPasswordInput', 'regPasswordButton')">Show</button>
                                 </div>
                             </div>
                         </div>
-
-                        <div class="form-group">
-                            <label for="password">{{ __('Confirm Password') }}</label>
                         
+                        <div class="form-group mb-3">
+                            <label for="password">{{ __('Confirm Password') }}</label>
+                            
                             <div class="input-group">
                                 <input id="regConfPasswordInput" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
                                 <div class="input-group-append">
@@ -89,14 +89,16 @@
                                 </div>
                             </div>
                         </div>
-                                               
-                        <button type="submit" class="btn btn-primary">{{ __('Register') }}</button>
+                        
+                        <div class="text-center">
+                            <button type="submit" class="btn btn-primary ">{{ __('Register') }}</button>
+                        </div>
                     </form>
                 </div>
                 
                 <div class="card-footer">
                     <div class="text-center">
-                        <a href="{{ route('login') }}">Already have an account? Login here</a>
+                        <a href="{{ route('login') }}" class="text-decoration-none">Already have an account? Login here</a>
                     </div>
                 </div>
             </div>
